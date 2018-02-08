a682e08a82a785238a2fde96b568616557999a3d
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-02-08 17:34:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function deleteFromObjectByKey(object, key) {
   return Object.assign({});
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
- object = delete object[key];
+ delete object[key];
  return object;
 }
