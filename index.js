28c838b789032962c8fbb651fe3f56df945454c2
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-02-08 17:02:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var recipes = {ingredient: "amount"};
 function updateObjectWithKeyAndValue(object, key, value) {
-return Object.assign({},{[key]: value});
+return Object.assign({}, object, {[key]: value});
 }
